1e1d2e99227ffcd7446e8a366af83dffe32b91d3
Vinícius Ribeiro <viniciusribeiro@ua.pt>
2020-05-14 17:18:19 +0100

    Linking Travis with Docker

--- .travis/main.sh
+++ .travis/main.sh
@@ -28,8 +28,18 @@ setup_dependencies() {
   Setting up dependencies.
   "
 
-  sudo apt update -y
-  sudo apt install --only-upgrade docker-ce=5:19.03.8~3-0~ubuntu-bionic -y
+  sudo apt-get update -y
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update -y
+  sudo apt install --only-upgrade docker-ce=5:19.03.8~3-0~ubuntu-bionic docker-ce-cli containerd.io -y
 
   docker info
 }
