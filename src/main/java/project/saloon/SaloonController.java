a4badf3e2647ee6f6469bd566e8abfb082225fe0
Alina <alina-yanchuk02@outlook.pt>
2020-05-31 19:33:21 +0100

    spring atualizado

--- src/main/java/project/saloon/SaloonController.java
+++ src/main/java/project/saloon/SaloonController.java
@@ -108,21 +108,57 @@ public class SaloonController {
         User user = new User("Alina", "alina@gmail.com", "ola");
         userRepository.save(user);
         Saloon saloon = new Saloon("Beldade", "7890", "Aveiro",
-                "Portugal", "Aberto", "Cabeleireiro", "12345",
-                "O melhor cabeleireiro de Portugal! Venha visitar-no!", "https://fotos.vivadecora.com.br/decoracao-salao-de-beleza-piso-de-porcelanato-claro-e-cadeira-de-couro-preta-revistavd-187516-square_cover_xsmall.jpg", "Rua D.Jose", user);
+                "Portugal", "Aberto", "Cabeleireiro", "235463928",
+                "Desde cortes extravagantes, a simples retoques, aqui encontrará o melhor atendimento e profissionalismo em Portugal.", "https://fotos.vivadecora.com.br/decoracao-salao-de-beleza-piso-de-porcelanato-claro-e-cadeira-de-couro-preta-revistavd-187516-square_cover_xsmall.jpg", "Rua D.José, 3100-450", user);
         Service service = new Service();
        
         service.setName("Corte de cabelo");
         service.setPrice(9.99);
         service.setAvailable("Disponível");
-        service.setDescription("Só para mulheres");
+        service.setDescription("Feminino e Masculino");
         saloonRepository.save(saloon);
         service.setSaloon(saloon);
     
         serviceRepository.save(service);
 
-        Saloon b=new Saloon("Machos","3100-480","Aveiro","Portugal","Aberto","Barbeiro","951882438","O melhor barbeiro de Portugal e arredores!","https://blog.mensmarket.com.br/wp-content/uploads/2019/01/salao-cabeleireiro-masculino-600x300.png","Rua Don José",user);
+        Service service_2 = new Service();
+       
+        service_2.setName("Pintura");
+        service_2.setPrice(40.99);
+        service_2.setAvailable("Disponível");
+        service_2.setDescription("Feminino e Masculino");
+        service_2.setSaloon(saloon);
+        serviceRepository.save(service_2);
+
+        Saloon b=new Saloon("Barberhood","3100-480","Lisboa","Portugal","Aberto","Barbeiro","951882438","Os serviços de Barberhood o distinguem para os cortes clássicos executados com domínio extremo e precisão nos detalhes. Para além de oferecer uma qualidade excelente nos tratamentos, eles oferecem também bebida e cervejas a todos os clientes.","https://barberhood.pt/wp-content/uploads/2019/11/cort.jpg","Av. Óscar Monteiro Torres 6A, 1000-219",user);
         saloonRepository.save(b);
+
+        Service service_3 = new Service();
+       
+        service_3.setName("Barba - navalha");
+        service_3.setPrice(8.99);
+        service_3.setAvailable("Disponível");
+        service_3.setDescription("Masculino");
+        service_3.setSaloon(b);
+        serviceRepository.save(service_3);
+
+        Service service_4 = new Service();
+       
+        service_4.setName("Corte de cabelo/Rapar");
+        service_4.setPrice(7.99);
+        service_4.setAvailable("Disponivel");
+        service_4.setDescription("Masculino");
+        service_4.setSaloon(b);
+        serviceRepository.save(service_4);
+
+        Service service_5 = new Service();
+       
+        service_5.setName("Combo (barba e corte)");
+        service_5.setPrice(15.99);
+        service_5.setAvailable("Disponivel");
+        service_5.setDescription("Masculino");
+        service_5.setSaloon(b);
+        serviceRepository.save(service_5);
    
         Schedule schedule1=new Schedule(new Time(12,0,0), new Time(13,0,0));
         schedule1.setService(service);
@@ -130,6 +166,12 @@ public class SaloonController {
         Schedule schedule2=new Schedule(new Time(14,0,0),new Time(15,0,0));
         schedule2.setService(service);
         scheduleRepository.save(schedule2);
+        Schedule schedule3=new Schedule(new Time(12,0,0), new Time(16,0,0));
+        schedule3.setService(service_2);
+        scheduleRepository.save(schedule3);
+        Schedule schedule4=new Schedule(new Time(17,0,0), new Time(18,0,0));
+        schedule4.setService(service_3);
+        scheduleRepository.save(schedule4);
 
       
 
