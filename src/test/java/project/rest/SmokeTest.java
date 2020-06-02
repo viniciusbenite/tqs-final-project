08b23a9bfcb2981a5935dc9d46709fd7ed24f7f4
Vinícius Ribeiro <viniciusribeiro@ua.pt>
2020-06-02 13:41:24 +0100

    SmokeTest bug fix

--- src/test/java/project/rest/SmokeTest.java
+++ src/test/java/project/rest/SmokeTest.java
@@ -3,6 +3,7 @@ package project.rest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import project.reservation.ReservationController;
 import project.saloon.SaloonController;
@@ -13,22 +14,28 @@ import project.user.UserController;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ContextConfiguration()
+@ContextConfiguration(classes = {ReservationController.class, ScheduleController.class, SaloonController.class,
+        UserController.class, ServiceController.class})
 public class SmokeTest {
 
     @Autowired
+    @MockBean
     private ReservationController reservationController;
 
     @Autowired
+    @MockBean
     private SaloonController saloonController;
 
     @Autowired
+    @MockBean
     private ScheduleController scheduleController;
 
     @Autowired
+    @MockBean
     private ServiceController serviceController;
 
     @Autowired
+    @MockBean
     private UserController userController;
 
     @Test
