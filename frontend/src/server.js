6c2dc9e23ef21c47868765b5bc3a2c27cd7519e6
Alina <alina-yanchuk02@outlook.pt>
2020-06-05 22:44:15 +0100

    adicionada pasta de reports

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,20 @@
-var cors = require('cors')
 
-app.use(cors()) // Use this after the variable declaration
+var cors = require('cors');
+
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  })
+const publicPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 3000;app.use(express.static(publicPath));app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});app.listen(port, () => {
+   console.log('Server is up!');
+});
+
+
+
+
